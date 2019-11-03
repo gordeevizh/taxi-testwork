a71286696d2c4d6a9516f91ce8244220e93372f9
Sergey Gordeev <gordeevizh@yandex.ru>
2019-11-03 16:41:20 +0400

    feat(config): config hotload and pcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.pcss';
 
 const App: React.FC = () => {
   return (
@@ -16,7 +16,7 @@ const App: React.FC = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React sdfsdf
+          Learn React
         </a>
       </header>
     </div>
